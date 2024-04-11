fd12b43a75b44ebc122dea74d44e78066c12554b
yataknemogy <savel2015@icloud.com>
2024-04-11 20:12:14 +0300

    add code

--- src/main/java/org/example/Calculate.java
+++ src/main/java/org/example/Calculate.java
@@ -1,2 +1,15 @@
-package org.example;public class Calculate {
+package org.example;
+import javax.swing.*;
+
+public class Calculate extends JFrame {
+  public static void main(String args){
+    new Calculate();
+  }
+
+  private Calculate(){
+    super("Калькулятор");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(400, 300);
+    setVisible(true);
+  }
 }
